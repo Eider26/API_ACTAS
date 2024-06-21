37e230a9f4ba7b41bb0a9a1b6c34bea39c8d31cf
CarlosMateoM <carlosmateo484@gmail.com>
2024-06-21 14:11:05 -0500

    add: actas crud methods

--- rutas.php
+++ rutas.php
@@ -5,12 +5,18 @@ $routes = [
         '/' => 'HomeController@index',
         '/users' => 'UsuariosControlador@index', // Ruta con middleware
         '/users/(\d+)' => 'UsuariosControlador@show', // Ruta con parámetro de ID
+        '/actas' => 'ActasControlador@index',
+        '/actas/(\d+)' => 'ActasControlador@show',
     ],
     'POST' => [
         '/users/create' => 'UserController@create',
         '/actas' => 'ActasControlador@store'
     ],
+    'PUT' => [
+        '/actas/(\d+)' => 'ActasControlador@update',
+    ],
     'DELETE' => [
         '/users/(\d+)' => 'UserController@delete@authorized', // Ruta con parámetro de ID
+        '/actas/(\d+)' => 'ActasControlador@destroy'
     ],
 ];
